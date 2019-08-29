b4d4dfa25382ea312fcaf51ebbec2bacdac6ec4c
COE-026 <coe-026@coe-026s-imac.local>
2019-08-29 15:31:28 +0530

    hello

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -9,6 +9,63 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    
+    
+    @IBOutlet weak var txtA: UITextField!
+    
+    
+    @IBOutlet weak var txtB: UITextField!
+    
+    
+    @IBAction func SegmentChange(_ sender: UISegmentedControl) {
+    
+        if txtA.text != "" || txtB.text != ""{
+            
+        
+        
+    let a = ((Int)(txtA.text!))!
+    let b = ((Int)(txtB.text!))!
+    var c = 0
+    
+        switch sender.selectedSegmentIndex {
+        case 0:
+            c = a + b
+        case 1:
+            c = a - b
+        case 2:
+            c = a * b
+        case 3:
+            c = a / b
+
+        default:
+            print("select method")
+            break
+        }
+        
+        let alertview = UIAlertController(title: "answer", message: "Answer is : \(c)", preferredStyle: .alert)
+        
+        alertview.addAction(UIAlertAction(title: "ok", style: .cancel, handler: { _ in
+            
+        }) )
+        self.present(alertview, animated: true, completion: nil)
+        
+    }
+        else {
+            let alertview = UIAlertController(title: "missing value", message: "addmissing value", preferredStyle: .alert)
+            
+            alertview.addAction(UIAlertAction(title: "ok", style: .cancel, handler: { _ in
+                
+            }) )
+            self.present(alertview, animated: true, completion: nil)
+            
+        }
+    }
+    
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
